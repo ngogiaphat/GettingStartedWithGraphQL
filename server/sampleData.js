c02533487e538e6b575fb8fae11f1064769bd8b3
Samuel Seo <ngogiaphat0802@gmail.com>
2023-07-04 17:44:32 +0700

    example data

---
+++ server/sampleData.js
@@ -0,0 +1,73 @@
+// Projects
+const projects = [
+  {
+    id: '1',
+    clientId: '1',
+    name: 'eCommerce Website',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'In Progress',
+  },
+  {
+    id: '2',
+    clientId: '2',
+    name: 'Dating App',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'In Progress',
+  },
+  {
+    id: '3',
+    clientId: '3',
+    name: 'SEO Project',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'In Progress',
+  },
+  {
+    id: '4',
+    clientId: '4',
+    name: 'Design Prototype',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'Done',
+  },
+  {
+    id: '5',
+    clientId: '5',
+    name: 'Auction Website',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'In Progress',
+  },
+];
+
+// Clients
+const clients = [
+  {
+    id: '1',
+    name: 'Trần Mai Vy',
+    email: 'maivy20202021@gmail.com',
+    phone: '0963653563',
+  },
+  {
+    id: '2',
+    name: 'Khưu Mỹ Xuyên',
+    email: 'khuumyxuyen11402@gmail.com',
+    phone: '0901856410',
+  },
+	{
+		id: '3',
+		name: 'Ngô Gia Phát',
+		email: 'ngogiaphat0802@gmail.com',
+		phone: '0933528678',
+	},
+  {
+    id: '4',
+    name: 'Trần Quốc Trọng',
+    email: 'tranquoctrong130202@gmail.com',
+    phone: '0357770753',
+  },
+  {
+    id: '5',
+    name: 'Nguyễn Huỳnh Đanh Khánh',
+    email: 'khanhvn3103@gmail.com',
+    phone: '0933792143',
+  },
+];
+module.exports = {projects, clients};
